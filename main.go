cbb6dcda811afd614a36c31625ef8c8bb2e3803b
Matthew North <mattnorth84@gmail.com>
2025-02-12 13:09:33 +0000

    implemented the user registration and login changes along with the command line user table reset as per the course

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"errors"
 	"fmt"
 	"github.com/MattInReality/gator/internal/config"
+	"github.com/MattInReality/gator/internal/database"
+	"github.com/google/uuid"
 	"log"
 	"os"
+	"time"
 )
 
+import _ "github.com/lib/pq"
+
 type state struct {
+	db     *database.Queries
 	config *config.Config
 }
 
@@ -23,11 +31,58 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 {
 		return errors.New("username is required")
 	}
-	err := s.config.SetUser(cmd.args[0])
+	ctx := context.TODO()
+	username := cmd.args[0]
+	_, err := s.db.GetUser(ctx, username)
+	if err != nil {
+		os.Exit(1)
+	}
+	err = s.config.SetUser(username)
 	if err != nil {
 		return err
 	}
 	fmt.Println("user has been set")
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) < 1 {
+		return errors.New("username is required to register")
+	}
+	username := cmd.args[0]
+	ctx := context.TODO()
+	uuid := uuid.New()
+	newUser := database.CreateUserParams{
+		ID:        uuid,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      username,
+	}
+	user, err := s.db.CreateUser(ctx, newUser)
+	if err != nil {
+		os.Exit(1)
+	}
+	err = s.config.SetUser(username)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("user was created %v", user)
+
+	return nil
+}
+
+func handlerReset(s *state, cmd command) error {
+	_ = cmd
+	ctx := context.TODO()
+	err := s.db.DeleteAllUsers(ctx)
+	if err != nil {
+		return fmt.Errorf("error reseting user table: %v", err)
+	}
+	err = s.config.SetUser("")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("reset user table successful")
 	return nil
 }
 
@@ -61,12 +116,22 @@ func main() {
 		log.Fatal("config could not be read - check file")
 		panic(1)
 	}
+	dbURL := configFile.DbUrl
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Println("issue with database connection")
+		os.Exit(1)
+	}
+	dbQueries := database.New(db)
 	appState := state{}
 	appState.config = &configFile
+	appState.db = dbQueries
 
 	cmds := commands{}
 	cmds.handlers = make(map[string]commandHandler)
 	cmds.handlers["login"] = handlerLogin
+	cmds.handlers["register"] = handlerRegister
+	cmds.handlers["reset"] = handlerReset
 
 	cmdArgs := os.Args
 	if len(cmdArgs) < 2 {
